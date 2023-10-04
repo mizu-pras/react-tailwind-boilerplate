dd2ee3e9d8543edf5a3b7aabc001ed5f80bc1024
Mizu Pras <1-dimas@hris-system.com>
2023-10-04 15:17:00 +0700

    init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default config;
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div className="bg-red-100 text-2xl">App</div>;
+};
+
+export default App;
